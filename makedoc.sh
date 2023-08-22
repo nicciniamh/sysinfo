18eef924c4a0988a7c88b4d4f8120da140501aea
Nicole Stevens <nicci@pi4>
2023-08-22 16:29:39 -0700

    Added up and down network speeds

--- makedoc.sh
+++ makedoc.sh
@@ -3,3 +3,4 @@ which pandoc >/dev/null 2>&1 || { echo 'pandoc is not installed'>&2; exit 1;}
 cp Readme.md sysinfo.1.md
 pandoc --standalone --to man sysinfo.1.md -o sysinfo.1
 pandoc --standalone --to html Readme.md -o sysinfo.html
+rm -f sysinfo.1.md
